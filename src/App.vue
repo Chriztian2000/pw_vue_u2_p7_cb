c8c0fc94f2288318f6a1c2a5aa1d371c9193be0e
Chriztian2000 <cabetancourt@uce.edu.ec>
2024-12-20 23:14:08 +0000

    taller18.1

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <Contador
+    encabezado="Primera"
+    :valor="calcular(1, 3)"
+    :esVerdad="muestra1"
+    :valor2="2"
+  />
+  <hr />
+  <Contador
+    encabezado="Segundo"
+    v-bind:valor="calcular(8, 2)"
+    :esVerdad="muestra2"
+  />
+  
+  <hr />  
+  <button @click="cambiar(1)">Cambiar Primero</button>
+  <button @click="cambiar(2)">Cambiar Segundo</button>
+</template>
+
+<script>
+import Contador from "./components/Contador.vue";
+export default {
+  name: "App",
+  components: {
+    Contador,
+  },
+
+  data() {
+    return {
+      muestra1: true,
+      muestra2: false,
+    };
+  },
+
+  methods: {
+    calcular(a, b) {
+      return a + b;
+    },
+    cambiar(valor) {
+      if (valor === 1) {
+        this.muestra1 = !this.muestra1;
+      } else {
+        this.muestra2 = !this.muestra2;
+      }
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Contador.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <h2 v-if="!encabezado.includes('a')">{{ encabezado }}: {{ valor2 }}</h2>
+    <p>{{ numero }}<sup>2</sup> = {{ calcularCuadradoComputado }}</p>
+    <p>{{ numero }}<sup>2</sup> = {{ calcularCuadradoComputado }}</p>
+    
+    <div>
+      <button @click="incrementar()">+1</button>
+      <button v-on:click="decrementar()">-1</button>    
+      
+    </div>
+
+    <div v-if="esVerdad">
+      <h1>FELIZ NAVIDAD</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      numero: this.valor,
+      titulo: "Contador",
+    };
+  },
+
+  methods: {
+    calcularCuadrado() {
+      console.log("Método calcularCuadrado");
+      return this.numero * this.numero;
+    },
+    incrementar() {
+      this.numero++;
+    },
+    decrementar() {
+      this.numero--;
+    },
+  },
+
+  computed: {
+    calcularCuadradoComputado() {
+      console.log("Método calcularCuadradoComputado");
+      return this.numero * this.numero;
+    },
+  },
+
+  props: {
+    encabezado: {
+      type: String,
+      required: true,
+      validator(value) {
+        return !value.includes('a');
+      },
+    },
+    valor: Number,
+    valor2: {
+      type: Number,
+      required: false,
+      default: 78,
+      validator(value) {
+        return value <= 77;
+      },
+    },
+    esVerdad: {
+      type: Boolean,
+      required: true,
+    },
+    arreglo: Array,
+    fecha: Date,
+    objetoPersona: Object,
+  },
+};
+</script>
+
+<style>
+button {
+  background: #64b687;
+  border-radius: 5px;
+  padding: 10px 10px;
+  border: 1px solid black;
+  color: white;
+  cursor: pointer;
+  margin: 0px 5px;
+  width: 100px;
+}
+
+button:hover {
+  background: #001a46;
+}
+</style>
