b103a782ace79054b21be942bf05002daf7b3250
Chriztian2000 <cabetancourt@uce.edu.ec>
2024-12-19 19:47:56 -0500

    taller18

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <Contador/>
-  <Contador/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <Contador
+    encabezado="Primero"
+    :valor="calcular(1, 3)"
+    :esVerdad="muestra1"
+    :valor2="2"
+  />
+  <hr />
+  <Contador
+    encabezado="Segundo"
+    v-bind:valor="calcular(8, 2)"
+    :esVerdad="muestra2"
+  />
+  
+  <hr />  
+  <button @click="cambiar(1)">Cambiar Primero</button>
+  <button @click="cambiar(2)">Cambiar Segundo</button>
 </template>
 
 <script>
-import Contador from './components/Contador.vue'
+import Contador from "./components/Contador.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Contador
-  }
-}
+    Contador,
+  },
+
+  data() {
+    return {
+      muestra1: true,
+      muestra2: false,
+    };
+  },
+
+  methods: {
+    calcular(a, b) {
+      return a + b;
+    },
+    cambiar(valor) {
+      if (valor === 1) {
+        this.muestra1 = !this.muestra1;
+      } else {
+        this.muestra2 = !this.muestra2;
+      }
+    },
+  },
+};
 </script>
 
 <style>
--- src/components/Contador.vue
+++ src/components/Contador.vue
@@ -1,22 +1,29 @@
 <template>
-  <h2>{{ titulo }}</h2>
+  <h2>{{ encabezado }}: {{ valor2 }}</h2>
   <p>{{ numero }}<sup>2</sup> = {{ calcularCuadradoComputado }}</p>
   <p>{{ numero }}<sup>2</sup> = {{ calcularCuadradoComputado }}</p>
-  <p>{{ numero }}<sup>2</sup> = {{ calcularCuadradoComputado }}</p>
-  <p>{{ numero }}<sup>2</sup> = {{ calcularCuadradoComputado }}</p>
-  <p>{{ numero }}<sup>2</sup> = {{ calcularCuadradoComputado }}</p>
+  
   <div>
-    <button v-on:click="incrementar()">+1</button>
+    <button @:click="incrementar()">+1</button>
     <button v-on:click="decrementar()">-1</button>
   </div>
+
+  <div v-if="esVerdad">
+    <h1>FELIZ NAVIDAD</h1>
+  </div>
+
 </template>
+
+
+<!-------------------------------------------------------->
 
 <script>
 export default {
   data() {
     return {
-      numero: 10,
+      numero: this.valor,
       titulo: "Contador",
+
     };
   },
 
@@ -36,14 +43,47 @@ export default {
   computed: {
     calcularCuadradoComputado() {
       console.log("Método calcularCuadradoComputado");
+
       return this.numero * this.numero;
     },
   },
+  //Primera forma de declarar props
+  //props: ['encabezado','valor'],
+
+  //Segunda forma de declarar props
+  props: {
+    encabezado: String,
+    valor: Number,
+    valor2: {
+      type: Number,
+      required: false,
+      default: 78,
+      validator(value){
+        return value <=77;
+      }
+    },
+    esVerdad: {
+      type: Boolean,
+      required: true,
+    },
+    arreglo: Array,
+    fecha: Date,
+    objetoPersona: Object,
+    
+
+  },
+  
+
+
+
 };
 </script>
 
-<style>
 
+<!-------------------------------------------------------->
+
+
+<style>
 button {
   background: #64b687;
   border-radius: 5px;
